a3a8270ae7d1c098f1f8133dc41c85f3df2cd16d
filiptaik <filip.taik@hotmail.com>
2020-05-01 13:12:21 +0300

    kirjeldus

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# LINQ
+# Filip Taik LINQ näidisrakendus
+
+Rakendus arvutab baari-inventuuri andmete järgi kui palju on mingit jooki puudu ja kui palju maksab puuduoleva kauba tellimine.
+
+Kõigepealt saab rakendus sisse faili inventuur.txt
+![](LINQ2.txt)
--- n2itrakendus/n2itrakendus/Program.cs
+++ n2itrakendus/n2itrakendus/Program.cs
@@ -12,7 +12,7 @@ namespace n2itrakendus
                              .Select(s => s.Split(' '))
                              .Select(m => new { nimetus = m[0], puudu = (((decimal.Parse(m[1])*0.01m) - (decimal.Parse(m[2])*0.01m))/10)*10
                               ,hind = int.Parse(m[3])
-                              , maksumus = Math.Floor(((decimal.Parse(m[1]) * 0.01m) - (decimal.Parse(m[2]) * 0.01m)) * decimal.Parse(m[3]) * 100)/100});
+                              ,maksumus = Math.Floor(((decimal.Parse(m[1]) * 0.01m) - (decimal.Parse(m[2]) * 0.01m)) * decimal.Parse(m[3]) * 100)/100});
             var kõrgeimhind = joogid.OrderBy(jook => -jook.maksumus);
 
             foreach (var jook in kõrgeimhind)
